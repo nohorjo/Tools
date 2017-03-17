b6b3e8a48218d5a13f68f0898702bbfdf33b2dbb
Muhammed Haque <nohorjo@gmail.com>
2017-03-17 15:14:12 +0000

    Changed calc to eval and to use javascript eval

--- java/src/Calc.java
+++ java/src/Calc.java
@@ -1,55 +1,17 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class Calc {
 	public static void main(String[] args) {
-		String expression;
+		ScriptEngineManager factory = new ScriptEngineManager();
+		ScriptEngine engine = factory.getEngineByName("JavaScript");
 		try {
-			expression = args[0];
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Usage:\n\t calc expression");
-			return;
+			String expression = String.join(" ", args);
+			engine.eval("print(eval('" + expression + "'))");
+		} catch (ScriptException e) {
+			e.printStackTrace();
 		}
-		String nums[] = expression.split("\\D");
-		String ops[] = expression.split("\\d");
-		Double eval = null;
-		int offset = 0;
-		for (int i = 1; i < ops.length; i++) {
-			String op = ops[i];
-			if (op.equals("")) {
-				offset++;
-				continue;
-			}
-			Double num2 = Double.parseDouble(nums[i - offset]);
-			if (eval == null) {
-				eval = Double.parseDouble(nums[(i - offset) - 1]);
-			}
-			eval = evaluate(eval, op, num2);
-
-		}
-		System.out.println(eval);
 	}
 
-	private static Double evaluate(Double a, String op, Double b) {
-		switch (op) {
-		case "/":
-			a /= b;
-			break;
-		case "*":
-			a *= b;
-			break;
-		case "-":
-			a -= b;
-			break;
-		case "+":
-			a += b;
-			break;
-		case "%":
-			a %= b;
-			break;
-		case "^":
-			a = Math.pow(a, b);
-			break;
-		default:
-			throw new UnsupportedOperationException("[" + op + "]");
-		}
-		return a;
-	}
 }
