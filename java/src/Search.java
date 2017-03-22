deb2dbd1ec9d3644f0c2868a1ca6d0e28d3ecb65
Muhammed Haque <nohorjo@gmail.com>
2017-03-22 12:37:46 +0000

    Added the ability to search contents of files

--- java/src/Search.java
+++ java/src/Search.java
@@ -1,18 +1,30 @@
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.regex.PatternSyntaxException;
 
+import nohorjo.cli.CLIArgs;
+
 public class Search {
+	private static CLIArgs cli;
+	private static String inFileRegex;
+
 	public static void main(String[] args) throws IOException {
 		try {
-			String path = args[0];
-			String regex = args[1];
+			cli = new CLIArgs(args);
+			String path = cli.getString("path", ".");
+			String regex = cli.getString("$1");
+
+			try {
+				inFileRegex = cli.getString("in-file");
+			} catch (NullPointerException e) {
+			}
 
 			search(regex, new File(path));
 
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Usage:\n\tsearch path pattern");
+		} catch (NullPointerException e) {
+			System.out.println("Usage:\n\tsearch [path=<path>] <file_name_pattern> [in-file=<search_pattern>]");
 		} catch (PatternSyntaxException e) {
 			System.err.println("Invalid pattern");
 		}
@@ -28,11 +40,16 @@ public class Search {
 		File[] files = file.listFiles(filter);
 		if (files != null) {
 			for (File f : files) {
-
+				String path = f.getCanonicalPath();
 				if (f.isDirectory()) {
 					search(regex, f);
+				} else if (inFileRegex != null) {
+					String contents = new String(Files.readAllBytes(f.toPath())).replaceAll("\\s", " ");
+					if (contents.matches(inFileRegex)) {
+						System.out.println("\r" + path);
+					}
 				} else {
-					System.out.println(f.getCanonicalPath());
+					System.out.println(path);
 				}
 			}
 		} else {
