48f82cf381c1af002995fc33b21418e80144e56d
Muhammed Haque <nohorjo@gmail.com>
2017-03-23 09:15:39 +0000

    Added verbose and literal search option

--- java/src/Search.java
+++ java/src/Search.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.UnmappableCharacterException;
 import java.nio.file.Files;
+import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 import nohorjo.cli.CLIArgs;
@@ -11,23 +12,26 @@ import nohorjo.cli.CLIArgs;
 public class Search {
 	private static String inFileRegex;
 	private static String startingPath;
+	private static boolean verbose;
 
 	public static void main(String[] args) throws IOException {
 		try {
 			CLIArgs cli = new CLIArgs(args);
 			File f = new File(cli.getString("path", "."));
 			startingPath = f.getCanonicalPath();
-			String regex = cli.getString("$1");
+			String regex = cli.getString("$1", ".*" + Pattern.quote(cli.getString("lit")) + ".*");
+			verbose = cli.getBoolean("verbose", false);
 
 			try {
-				inFileRegex = cli.getString("in-file");
+				inFileRegex = cli.getString("in-file", ".*" + Pattern.quote(cli.getString("in-file-lit")) + ".*");
 			} catch (NullPointerException e) {
 			}
 
 			search(regex, f);
 
 		} catch (NullPointerException e) {
-			System.out.println("Usage:\n\tsearch [path=<path>] <file_name_pattern> [in-file=<search_pattern>]");
+			System.out.println(
+					"Usage:\n\tsearch [path=<path>] (<file_name_pattern> | lit=<literal_search>) [in-file=<search_pattern> | in-file-lit=<literal_search>] [verbose=(true|false)]");
 		} catch (PatternSyntaxException e) {
 			System.err.println("Invalid pattern");
 		}
@@ -45,8 +49,14 @@ public class Search {
 			for (File f : files) {
 				String path = f.getCanonicalPath().replace(startingPath, "");
 				if (f.isDirectory()) {
+					if (verbose) {
+						System.out.print("Searching in " + path + "\r");
+					}
 					search(regex, f);
 				} else if (inFileRegex != null) {
+					if (verbose) {
+						System.out.print("Searching file: " + path + "\r");
+					}
 					int ln = 0;
 					try {
 						for (String line : Files.readAllLines(f.toPath(), Charset.defaultCharset())) {
