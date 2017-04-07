35fcc06461a1a81d3335e8a7742bd0b1ee9c1398
Muhammed Haque <nohorjo@gmail.com>
2017-04-07 12:03:31 +0100

    Added verbose option and changed syntax

--- java/src/Pipe.java
+++ java/src/Pipe.java
@@ -1,44 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+
+import nohorjo.cli.CLIArgs;
+import nohorjo.cli.InvalidCLIArgException;
 
 public class Pipe {
 	public static void main(String[] args) throws IOException {
-		if (args.length == 0) {
-			System.out.println("Usage:\n\tpipe commands...");
-			System.out.println("\nIn command args $out$ will be replaced by the std output of the last command");
-			return;
-		}
-		String pathext[] = (";" + System.getenv("PATHEXT")).split(";");
+		CLIArgs cli = new CLIArgs(args);
+		boolean verbose = cli.getBoolean("verbose", false);
+		try {
+			List<String> commands = cli.getList("commands", "|");
 
-		Runtime r = Runtime.getRuntime();
-		String outOfLast = "";
-		for (String command : args) {
-			Process p = null;
-			for (int i = 0; i < pathext.length; i++) {
-				String ext = pathext[i];
-				try {
-					p = r.exec(command.replaceFirst(" ", ext + " ").replace("$out$", outOfLast));
-					break;
-				} catch (IOException e) {
-					if (!e.getMessage().contains("CreateProcess error=2") || i + 1 == pathext.length) {
-						System.err.println("Cannot find program: " + command.split(" ")[0]);
-						return;
+			String pathext[] = (";" + System.getenv("PATHEXT")).split(";");
+
+			Runtime r = Runtime.getRuntime();
+			String outOfLast = "";
+			for (String command : commands) {
+				Process p = null;
+				for (int i = 0; i < pathext.length; i++) {
+					String ext = pathext[i];
+					String commandToExecute = command.trim().replaceFirst(" ", ext + " ").replace("$out$", outOfLast);
+					try {
+						p = r.exec(commandToExecute);
+						if (verbose) {
+							System.out.println("Executed:\n" + commandToExecute);
+						}
+						break;
+					} catch (IOException e) {
+						if (!e.getMessage().contains("CreateProcess error=2") || i + 1 == pathext.length) {
+							System.err.println("Cannot find program: " + command.split(" ")[0]);
+							return;
+						}
+					}
+				}
+				outOfLast = "";
+				try (InputStreamReader out = new InputStreamReader(p.getInputStream());
+						BufferedReader err = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
+
+					for (String line; (line = err.readLine()) != null;) {
+						System.out.println(line);
+					}
+					for (int d; (d = out.read()) != -1;) {
+						outOfLast += (char) d;
 					}
 				}
 			}
-			outOfLast = "";
-			try (InputStreamReader out = new InputStreamReader(p.getInputStream());
-					BufferedReader err = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
-
-				for (String line; (line = err.readLine()) != null;) {
-					System.out.println(line);
-				}
-				for (int d; (d = out.read()) != -1;) {
-					outOfLast += (char) d;
-				}
-			}
+			System.out.println(outOfLast);
+		} catch (InvalidCLIArgException e2) {
+			System.out.println("Usage:\n\tpipe commands=<list of commands seperated by | > [options...]");
+			System.out.println("\nIn command args $out$ will be replaced by the std output of the last command");
+			System.out.println("\nOptions:");
+			System.out.print("verbose\t");
+			System.out.println("(true|false) flag to set verbose output");
+			return;
 		}
-		System.out.println(outOfLast);
 	}
 }
